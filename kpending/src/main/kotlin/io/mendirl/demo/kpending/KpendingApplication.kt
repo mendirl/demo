74662ecee984169ae87f5274efa8272bfd74f69e
Fabien Couillard <couillard.fabien+dev@gmail.com>
2023-11-07 00:34:25 +0100

    add kpending

---
+++ kpending/src/main/kotlin/io/mendirl/demo/kpending/KpendingApplication.kt
@@ -0,0 +1,11 @@
+package io.mendirl.demo.kpending
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KpendingApplication
+
+fun main(args: Array<String>) {
+	runApplication<KpendingApplication>(*args)
+}
